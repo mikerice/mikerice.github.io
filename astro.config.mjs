d6f33de74487fe214b08afc7b362314140921e9a
Mike Rice <mike.hj.rice@gmail.com>
2025-03-10 20:58:36 -0700

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
@@ -7,12 +7,15 @@ import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bartoszlenar.github.io',
+  site: 'https://mikerice.github.io',
   base: '/',
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
       theme: 'css-variables',
     },
+  },
+  image: {
+    service: passthroughImageService(),
   },
 });
